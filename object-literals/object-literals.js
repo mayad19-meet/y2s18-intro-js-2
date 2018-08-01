7ef37a33da2b352d4060756e271567836b2c978d
meet <you@example.com>
2018-08-01 14:10:31 -0400

    hi

--- object-literals/object-literals.js
+++ object-literals/object-literals.js
@@ -1,1 +1,9 @@
-// Create your 'me' object literal here!
+var me = {
+	firstName: "Maya",
+	lastName:"Daniel",
+	age: 16,
+	favoriteFoods: ["pasta", "pizza"],
+	picture:"https://www.sbs.com.au/popasia/blog/2018/02/05/someone-turned-spongebob-squarepants-anime-and-its-amazing",
+}	
+document.write(JSON.stringify(<me>))
+
