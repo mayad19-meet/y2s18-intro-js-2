c76def9b330d31bd89f355dc7bb76315e607ddeb
meet <you@example.com>
2018-08-02 16:47:27 -0400

    hi

--- event-listeners/event-listeners.js
+++ event-listeners/event-listeners.js
@@ -1,7 +1,10 @@
-// YOUR TASK: Add more pictures!
-var pictures = ['./imgs/dog.jpg'];
-var currentIndex = 0;
 
+var pictures = ["./imgs/dog.jpg", "./imgs/mrkrab.png", "./imgs/Patrick_Star.png", "./imgs/spongebob_feat.jpg", "./imgs/Squee.png"];
+var currentIndex = 0;
+document.getElementsByTagName("img")[0].addEventListener("click", function(event) {
+showNextPicture();
+});
+showNextPicture();
 function showNextPicture() {
   currentIndex++; // increment current picture
   // if currentIndex is too large, start from the beginning again
@@ -9,5 +12,9 @@ function showNextPicture() {
     currentIndex = 0;
   }
 
-  // YOUR TASK: Finish this function!
+ 
+else if (currentIndex<pictures.length){
+var pic1=document.getElementsByTagName("img")[0];
+pic1.src=pictures[currentIndex];
+}
 }
